41274e7bd2e8bbc405b0bfe6b3098f42e43037c7
Werstef <32906385+Werstef@users.noreply.github.com>
2018-01-16 21:48:43 +0200

    python

---
+++ Python/p1.py
@@ -0,0 +1,13 @@
+#Lab1 II 5
+for i in range(1, 11):
+    print("Efrem Ion Stefan")
+
+#Lab1 II 6
+for i in range(1, 13):
+    print(i, i ** 2)
+
+#Lab1 I 4
+a = 5
+b = 4
+
+print(a+b, a-b, a*b, int(a/b), a/b, a%b )
