16993ee5cc0922aa147b1771f041af5f77286902
Werstef <32906385+Werstef@users.noreply.github.com>
2018-01-17 18:35:07 +0200

    update lab 8

---
+++ Lab8/L7-II-1.c
@@ -0,0 +1,74 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+
+
+
+ typedef struct list{
+
+    int data;
+    struct list* next;
+ } list;
+
+ void InsertNth(list *head, int *n){
+    int s;
+    printf("Se citeste index-ul la care se pune valoare : ");
+    scanf("%d", &s);
+    printf("\n");
+    list *temp = head;
+    list *random =(list*)malloc(sizeof(list));
+    printf("Se citeste nr pus la index-ul s: ");
+    scanf ("%d", &random->data);
+    printf("\n");
+    int i;
+    for(i=0; i<s - 2; i++)
+        temp = temp->next;
+
+    random->next = temp->next;
+    temp->next = random;
+
+    temp = head;
+
+    while(temp != NULL){
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+ }
+
+ int main () {
+
+
+    list *head, *tail, *temp;
+    head = NULL;
+    int i, n;
+    scanf("%d", &n);
+
+    for(i=0; i<n; i++){
+
+    temp = (list*)malloc(sizeof(list));
+    temp->next = NULL;
+    scanf("%d", &temp->data);
+
+    if(head == NULL)
+        head = temp;
+    else
+        tail->next = temp;
+
+    tail = temp;
+    }
+
+    InsertNth(head, n);
+    temp = head;
+
+    printf("\n");
+
+    while(temp != NULL){
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+
+
+
+
+ }
